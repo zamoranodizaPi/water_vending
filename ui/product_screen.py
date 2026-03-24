743a9aee76598ec3912889bb7f2cc8a4c8c12c0e
DIZA <diza@MacBookPro.lan>
2026-03-24 13:01:35 -0700

    Keep centered price when cards are disabled

--- ui/product_screen.py
+++ ui/product_screen.py
@@ -227,7 +227,7 @@ class ProductCard(QFrame):
     def _apply_state(self):
         self.setProperty("selected", self._selected)
         self.setProperty("affordable", self._affordable)
-        show_corner_price = self._has_active_selection and not self._selected
+        show_corner_price = self._has_active_selection and not self._selected and self._interactive
         self.price.setVisible(not show_corner_price)
         self.price_corner.setVisible(show_corner_price)
         self.setWindowOpacity(1.0 if self._interactive else 0.72)
